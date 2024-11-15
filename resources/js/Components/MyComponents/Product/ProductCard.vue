6fc68b89e86cdb1738e04c167ad56dfea9116b8a
Angel Vazquez <angelvazquez470@gmail.com>
2024-11-14 18:26:47 -0600

    working on products routes

---
+++ resources/js/Components/MyComponents/Product/ProductCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <main class="rounded-2xl border-2 border-[#D9D9D9] p-4 h-80 relative">
+
+        <!-- Botones de acción -->
+        <div class="absolute top-2 right-2 flex items-center justify-end space-x-2">
+            <button class="rounded-full size-6 bg-primary text-white flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+            </button>
+            <button class="rounded-full size-6 bg-primary text-white flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+            </button>
+        </div>
+
+        <figure class="flex items-center justify-center h-1/2 w-full">
+            <img class="object-contain h-full" :src="product.media[0].original_url" :alt="product.name">
+        </figure>
+
+        <div class="text-center mt-4">
+            <h2 class="font-bold">{{ product.name }}</h2>
+            <h1 class="text-[13px] mt-3 line-clamp-2">{{ product.description }}</h1>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+
+    }
+},
+components:{
+
+},
+props:{
+product: Object
+},
+methods:{
+
+}
+}
+</script>
